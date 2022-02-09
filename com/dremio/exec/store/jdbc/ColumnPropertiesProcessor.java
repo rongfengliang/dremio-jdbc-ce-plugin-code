a58cef63a6bd2f37d632db24c75e378eb351099c
rong fengliang <1141591465@qq.com>
2022-02-09 15:03:57 +0800

    delte

--- com/dremio/exec/store/jdbc/ColumnPropertiesProcessor.java
+++
@@ -1,7 +0,0 @@
-package com.dremio.exec.store.jdbc;
-
-import com.dremio.exec.store.jdbc.JdbcFetcherProto.ColumnProperties.Builder;
-
-public interface ColumnPropertiesProcessor {
-   void process(Builder var1);
-}
--- com/dremio/exec/store/jdbc/conf/DialectConf.java
+++
@@ -1,9 +0,0 @@
-package com.dremio.exec.store.jdbc.conf;
-
-import com.dremio.exec.catalog.conf.ConnectionConf;
-import com.dremio.exec.store.StoragePlugin;
-import com.dremio.exec.store.jdbc.dialect.JdbcDremioSqlDialect;
-
-public abstract class DialectConf<T extends DialectConf<T, P>, P extends StoragePlugin> extends ConnectionConf<T, P> {
-   public abstract JdbcDremioSqlDialect getDialect();
-}
